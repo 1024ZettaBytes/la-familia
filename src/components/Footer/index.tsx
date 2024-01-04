2344b42f7de30f34854f24a3e2c2f06a4c4ba6a9
Eduardo Ramirez <ramirezcota.eduardo@gmail.com>
2024-01-03 19:48:59 -0700

    Fixed footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -18,7 +18,7 @@ function Footer() {
       >
         <Box>
           <Typography variant="subtitle1">
-            &copy; {new Date().getFullYear()} - Servi Hogar: Soluciones a tu medida.
+            &copy; {new Date().getFullYear()} - Lavadoras "La familia": Unidos para servirte.
           </Typography>
         </Box>
       </Box>
